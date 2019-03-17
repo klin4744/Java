3b2c80673353f11a9ebcf9b1dfe47503e0723a28
klin4744 <klin4744@bths.edu>
2019-03-16 22:56:34 -0400

    Create DataFormat.java

---
+++ Assignments/DataFormat.java
@@ -0,0 +1,17 @@
+
+public class DataFormat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x;
+		// 
+		x = 25.0/4.0;
+		System.out.println(x);
+		x = 25/4;
+		System.out.println(x);
+		x = 25/4;
+		System.out.println(x);
+
+	}
+
+}
