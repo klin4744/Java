e835c63844dde01e7f27a06629d39fb3230e16c5
klin4744 <klin4744@bths.edu>
2019-03-17 22:13:06 -0400

    Update HW2_2.java

--- Assignments/HW2_2.java
+++ Assignments/HW2_2.java
@@ -1,25 +1,51 @@
+
+// Kevin Lin 110277459
 import java.util.Scanner;
-public class HW2_2 {
+
+public class Homework2_2 {
 
 	public static void main(String[] args) {
-		Scanner stdin = new Scanner(System.in);
+		// TODO Auto-generated method stub
 		String str1 = "", str2 = "";
-		HW2_2 obj1 = new HW2_2();
-		HW2_2 obj2 = new HW2_2();
-		System.out.println("Please enter the first string: ");
+		Scanner stdin = new Scanner(System.in);
+		System.out.print("Please enter the first string: ");
 		str1 = stdin.nextLine();
-		System.out.println("Please enter the second string: ");
+		System.out.print("Please enter the second string: ");
 		str2 = stdin.nextLine();
-		if(str1.length() != str2.length()) {
-			System.out.println(str1 + " " + str2 + " are NOT anagrams" );
-		}else {
-			for(int i=0; i<str1.length(); i++) {
-					
+		stdin.close();
+		if (anagram(str1, str2)) {
+			System.out.println(str1 + " and " + str2 + " are anagrams");
+		} else {
+			System.out.println(str1 + " and " + str2 + " are NOT anagrams");
+		}
+	}
+
+	public static boolean anagram(String str1, String str2) {
+		str1 = str1.toLowerCase();
+		str2 = str2.toLowerCase();
+		int currentCharCount = 1, match = 0;
+		for (int i = 0; i < str1.length(); i++) {
+			if (str1.indexOf(str1.charAt(i)) != i) {
+				continue;
+			}
+			for (int j = i + 1; j < str1.length(); j++) {
+				if (str1.charAt(j) == str1.charAt(i)) {
+					currentCharCount++;
 				}
 			}
-		}
-		
+			for (int k = 0; k < str2.length(); k++) {
+				if (str2.charAt(k) == str1.charAt(i)) {
+					match++;
+				}
+			}
+			if (currentCharCount != match) {
+				return false;
+			}
+			currentCharCount = 1;
+			match = 0;
 
+		}
+		return true;
 	}
 
 }
